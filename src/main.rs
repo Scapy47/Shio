c5bc6a49aefbed5e28f17d72497144b837f6417b
Scapy47 <134828740+Scapy47@users.noreply.github.com>
2025-10-09 18:34:07 +0000

    sending first network request with ureq

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 use clap::Parser;
+// use serde::Deserialize;
+use ureq;
 
 #[derive(Parser, Debug)]
 #[command(version, about)]
@@ -6,7 +8,28 @@ struct Args {
     name: String,
 }
 
+// #[derive(Deserialize)]
+// struct MyRecvBody {
+//     #[serde(rename = "userId")]
+//     user_id: u32,
+//     id: u32,
+//     title: String,
+//     body: String,
+// }
+
 fn main() {
     let args = Args::parse();
     println!("This is the start of {}!!", args.name);
+    net_hello();
+}
+
+fn net_hello() {
+    let res = ureq::get("https://jsonplaceholder.typicode.com/posts/1")
+        .header("Accept", "application/json")
+        .call()
+        .unwrap()
+        .status()
+        .to_string();
+
+    println!("{res}");
 }

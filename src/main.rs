1a7f311a57577a4b3bc3dc80d6f53aaa7a213a45
scapy47 <134828740+Scapy47@users.noreply.github.com>
2026-01-29 21:53:05 +0530

    rendering the table correct

--- src/main.rs
+++ src/main.rs
@@ -4,16 +4,21 @@ use ratatui::{
     crossterm::event::{self, Event},
     layout::{Constraint, Layout},
     style::Style,
-    widgets::{Block, BorderType, Paragraph},
+    widgets::{Block, BorderType, Paragraph, Row, Table},
 };
 use serde::Deserialize;
 use serde_json::Value;
-use std::{collections::HashMap, time::Duration};
+use std::{
+    collections::HashMap,
+    sync::{Arc, mpsc},
+    thread,
+    time::Duration,
+};
 use tui_input::{Input, backend::crossterm::EventHandler};
 use ureq::{Agent, RequestBuilder, typestate::WithoutBody};
 
 mod utils;
-use crate::utils::{LOGO, decrypt_url};
+use crate::utils::{ASCII_ART, decrypt_url};
 
 #[derive(Parser, Debug)]
 struct Args {
@@ -109,7 +114,10 @@ impl Api {
             .query("query", gql)
     }
 
-    fn search_anime(&self, query: &str) -> Result<SearchResponse, Box<dyn std::error::Error>> {
+    fn search_anime(
+        &self,
+        query: String,
+    ) -> Result<SearchResponse, Box<dyn std::error::Error + Send + Sync>> {
         let gql = "query( $search: SearchInput $limit: Int $page: Int $translationType: VaildTranslationTypeEnumType $countryOrigin: VaildCountryOriginEnumType ) { shows( search: $search limit: $limit page: $page translationType: $translationType countryOrigin: $countryOrigin ) { edges { _id name availableEpisodes __typename } }}".to_string();
 
         let variables_json = format!(
@@ -196,23 +204,37 @@ impl Api {
 struct App {
     args: Args,
     input: Input,
+    api: Arc<Api>,
+    search_resp: Option<SearchResponse>,
     exit: bool,
 }
 impl App {
     fn new() -> Self {
+        let args = Args::parse();
+        let api = Arc::new(Api::new());
+
         App {
-            args: Args::parse(),
+            args: args,
             input: Input::default(),
+            api: api,
+            search_resp: None,
             exit: false,
         }
     }
 
     fn main_loop(&mut self, terminal: &mut DefaultTerminal) -> std::io::Result<()> {
+        let (tx, rx) = mpsc::channel();
+        let api_clone = self.api.clone();
+        let name = self.args.name.clone();
+
+        thread::spawn(move || tx.send(api_clone.search_anime(name).unwrap()));
+
         while !self.exit {
+            if let Ok(resp) = rx.recv() {
+                self.search_resp = Some(resp);
+            }
+
             terminal.draw(|frame| self.render(frame))?;
-            // if crossterm::event::read()?.is_key_press() {
-            //     break;
-            // }
             let event = event::read()?;
             if let Event::Key(key) = event {
                 match key.code {
@@ -226,14 +248,15 @@ impl App {
         Ok(())
     }
 
-    fn render(&self, frame: &mut Frame) {
-        let api = Api::new();
-
+    fn render(&mut self, frame: &mut Frame) {
         let [top, bottom] =
             Layout::vertical([Constraint::Percentage(10), Constraint::Fill(1)]).areas(frame.area());
 
         let [bottom_left, bottom_right] =
             Layout::horizontal([Constraint::Percentage(70), Constraint::Fill(1)]).areas(bottom);
+
+        let [bottom_right_top, bottom_right_bottom] =
+            Layout::vertical([Constraint::Percentage(50), Constraint::Fill(1)]).areas(bottom_right);
 
         let input = Paragraph::new(self.input.value()).block(
             Block::bordered()
@@ -243,91 +266,60 @@ impl App {
         );
         frame.render_widget(input, top);
 
+        let rows = if let Some(resp) = &self.search_resp {
+            let mut list = vec![];
+            list.insert(0, Row::new(vec!["Name", "id", "type", "number of ep"]));
+            for anime in &resp.data.shows.edges {
+                let ep_count = anime
+                    .available_episodes
+                    .get("sub")
+                    .and_then(|v| v.as_u64())
+                    .unwrap_or(0);
+
+                list.push(Row::new(vec![
+                    anime.name.clone(),
+                    anime.id.clone(),
+                    anime.typename.clone(),
+                    ep_count.to_string(),
+                ]));
+            }
+            list
+        } else {
+            vec![Row::new(vec!["loading..."])]
+        };
+
         let block_left = Block::bordered()
             .title("")
             .border_type(BorderType::Rounded)
             .style(Style::new().red());
-        frame.render_widget(block_left, bottom_left);
+        frame.render_widget(&block_left, bottom_left);
+        frame.render_widget(
+            Table::new(
+                rows,
+                [
+                    Constraint::Percentage(25),
+                    Constraint::Percentage(25),
+                    Constraint::Percentage(25),
+                    Constraint::Percentage(25),
+                ],
+            ),
+            block_left.inner(bottom_left),
+        );
 
         let block_right = Block::bordered()
             .border_type(BorderType::Rounded)
             .style(Style::new().magenta());
         frame.render_widget(&block_right, bottom_right);
-
-        let [bottom_right_top, bottom_right_bottom] =
-            Layout::vertical([Constraint::Percentage(50), Constraint::Fill(1)]).areas(bottom_right);
-
+        frame.render_widget(
+            Paragraph::new(ASCII_ART).centered(),
+            block_right.inner(bottom_right_top),
+        );
         frame.render_widget(
             Block::bordered()
                 .border_type(BorderType::Rounded)
                 .style(Style::new().blue()),
             block_right.inner(bottom_right_bottom),
         );
-        frame.render_widget(
-            Paragraph::new(LOGO).centered(),
-            block_right.inner(bottom_right_top),
-        );
-
-        // frame.render_widget(
-        //     Paragraph::new("Hello firend").block(Block::bordered()),
-        //     frame.area(),
-        // );
-
-        // let mut row = vec![];
-        // let resp = api.search_anime(&self.args.name).unwrap();
-        // for anime in resp.data.shows.edges {
-        //     let ep_count = anime
-        //         .available_episodes
-        //         .get("sub")
-        //         .and_then(|v| v.as_u64())
-        //         .unwrap_or(0);
-        //
-        //     row.push(Row::new(vec![
-        //         anime.name,
-        //         anime.typename,
-        //         ep_count.to_string(),
-        //         anime.id,
-        //     ]));
-        // }
-        //
-        // row.insert(
-        //     0,
-        //     Row::new(vec!["Name", "Type", "Episodes", "ID"]).style(Style::new().red()),
-        // );
-        //
-        // let outer_area = frame.area();
-        // let outer_block = Block::bordered()
-        //     .border_type(BorderType::Rounded)
-        //     .border_style(Style::new().red())
-        //     .title("shio")
-        //     .title_alignment(HorizontalAlignment::Center);
-        //
-        // let inner_area = outer_block.inner(outer_area);
-        // let layout_inner = Layout::default()
-        //     .direction(Direction::Horizontal)
-        //     .constraints(vec![Constraint::Percentage(70), Constraint::Percentage(30)])
-        //     .split(inner_area);
-        //
-        // frame.render_widget(outer_block, outer_area);
-        //
-        // frame.render_widget(
-        //     Table::new(
-        //         row,
-        //         [
-        //             Constraint::Percentage(70),
-        //             Constraint::Percentage(5),
-        //             Constraint::Percentage(10),
-        //             Constraint::Percentage(15),
-        //         ],
-        //     )
-        //     .block(Block::bordered()),
-        //     layout_inner[0],
-        // );
-
-        //     frame.render_widget(
-        //         Paragraph::new("test").block(Block::bordered()),
-        //         layout_inner[1],
-        //     );
     }
 }
 
